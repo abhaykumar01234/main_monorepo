04766557974e289e84255cd8ace713beef07869c
abhakuma <abhakuma@c02f5501md6m.ent.gartner.com>
2021-04-09 17:17:02 +0530

    Blue shadow

--- packages/shadowizard/index.js
+++ packages/shadowizard/index.js
@@ -5,7 +5,7 @@ function shadowizard(options) {
   else options.shadow_type = "15px";
 
   images.forEach((img) => {
-    img.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(255,0,0,0.45)`;
+    img.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,255,255,0.45)`;
     if (options.padding) img.style.padding = "1em";
   });
 }
